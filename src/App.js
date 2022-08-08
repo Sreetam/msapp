68c9e27dff3a60c1436b32fd9e085d614c8a077f
Sreetam <fearthesreetam@gmail.com>
2022-08-08 17:47:55 +0530

    Stable running

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import './App.css';
 
 function App() {
-  let shoppingDone = false;
-  let childsAllowance;
-
-  if (shoppingDone === true) {
-    childsAllowance = 10;
-  } else {
-    childsAllowance = 5;
-  }
+  let gamestate = 0;
+  const handleClick = () => { gamestate = (gamestate + 1 ) % 2; console.log(gamestate); }
   return (
-    <div>
-      <h1>Number guessing game</h1>
-
-      <p>We have selected a random number between 1 and 100. See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</p>
-
-      <div class="form">
-        <label for="guessField">Enter a guess: </label>
-        <input type="number" min="1" max="100" required id="guessField" class="guessField"></input>
-        <input type="submit" value="Submit guess" class="guessSubmit"></input>
-      </div>
-
-      <div class="resultParas">
-        <p class="guesses"></p>
-        <p class="lastResult"></p>
-        <p class="lowOrHi"></p>
-      </div>
-    </div>
+    <>
+      <button class="guessSubmit" onClick={handleClick}>{gamestate}</button>
+    </>
   );
 }
 
