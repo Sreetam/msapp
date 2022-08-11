3203a85111a993925efb2c2487d8e519c1de650b
Sreetam <fearthesreetam@gmail.com>
2022-08-11 17:41:23 +0530

    involve bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  let gamestate = 0;
-  const handleClick = () => { gamestate = (gamestate + 1 ) % 2; console.log(gamestate); }
-  return (
-    <>
-      <button class="guessSubmit" onClick={handleClick}>{gamestate}</button>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { gamestate: "Positive", color: "btn-success" }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    if (this.state.gamestate === "Positive") {
+      this.setState({ gamestate: "Negative", color: "btn-danger" })
+    }
+    else {
+      this.setState({ gamestate: "Positive", color: "btn-success" })
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className='container-fluid'>
+          <div className='container'>
+            <div className="card">
+              <div className="card-header">
+                Trinity
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">React App</h5>
+                <p className="card-text">Out Content</p>
+                <button className={"btn " + this.state.color} onClick={this.handleClick}>
+                  {this.state.gamestate}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
